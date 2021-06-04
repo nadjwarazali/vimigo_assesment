9c113b2b7e13b0d66baff0f990e35660b9f6b285
Nadjwa Razali <57883875+nadjwarazali@users.noreply.github.com>
2021-06-04 19:32:03 +0800

    update

--- lib/my_contact/contact.dart
+++ lib/my_contact/contact.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:share/share.dart';
-import 'contact_list.dart';
-import 'contact_list.dart';
+import 'package:vimigo_assessment/my_contact/search_widget.dart';
 import 'contact_model.dart';
 import 'package:intl/intl.dart';
 import 'package:timeago/timeago.dart' as timeago;
-
-import 'contact_model.dart';
-import 'contact_model.dart';
-
+import 'package:flutter/cupertino.dart';
 
 class ContactWithList extends StatefulWidget {
   final Contact contact;
@@ -21,10 +17,23 @@ class ContactWithList extends StatefulWidget {
 
 class _ContactWithListState extends State<ContactWithList> {
   bool isSwitched = true;
+  List<Contact> contacts = [];
+  String query = '';
+  GlobalKey<RefreshIndicatorState> refreshKey;
+  int itemCount = 15;
+
+  @override
+  void initState() {
+    super.initState();
+    contacts = allContacts;
+    refreshKey = GlobalKey<RefreshIndicatorState>();
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
         backgroundColor: Colors.white,
         appBar: AppBar(
           elevation: 0,
@@ -32,90 +41,125 @@ class _ContactWithListState extends State<ContactWithList> {
           actions: [
             buildSwitchListTile(),
             IconButton(
-              icon: Icon(Icons.sort, color: Colors.purple),
-              onPressed: () {
-                setState(() {
-                  contacts
-                      .map((Contact contacts) => Card(
-                    child: ListTile(
-                      title: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(contacts.user, style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16)),
-                          Text(toggleSwitch(isSwitched, contacts)),
-                        ],
-                      ),
-                      subtitle: Text("0" + contacts.phone.toString()),
-                      onTap: () => share(context, contacts),
-                    ),
-                  )).toList();
-                  contacts.sort((a,b) => b.checkIn.compareTo(a.checkIn));
-                });
-              },
-            ),
+                icon: Icon(Icons.sort, color: Colors.purple),
+                onPressed: () {
+                  sortContact(context);
+                }),
           ],
         ),
-        body: SingleChildScrollView(
-          // child: _buildInboxList(context, contacts)
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 30),
-              Padding(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 30),
+            Padding(
                 padding: EdgeInsets.all(20),
                 child: Text(
-                  "My Contacts",style: TextStyle(
+                  "My Contacts",
+                  style: TextStyle(
                     color: Colors.purple,
                     fontWeight: FontWeight.bold,
                     fontSize: 40,
-                ), textAlign: TextAlign.left,
-                )
-              ),
-              SizedBox(height: 20 / 4),
-              Divider(),
-              Column(
-                  children: contacts
-                      .map((Contact contacts) => Container(
-                    decoration: BoxDecoration(
-                      border: Border(bottom: BorderSide(width: 0.05))
-                    ),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                          child: ListTile(
-                            title: Padding(
-                              padding: const EdgeInsets.only(bottom: 10),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(contacts.user, style: TextStyle(
-                                      color: Colors.purple,
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: 18)),
-                                  Text(toggleSwitch(isSwitched, contacts),  style: TextStyle(
-                                      color: Colors.grey,
-                                      fontSize: 14)),
-                                ],
-                              ),
-                            ),
-                            subtitle: Text("0" + contacts.phone.toString(), style: TextStyle(
-                                color: Colors.grey,
-
-                                fontSize: 16)),
-                            onTap: () => share(context, contacts),
-                          ),
-                        ),
-                      ))
-                      .toList()
-
-              ),
-            ],
-          ),
+                  ),
+                  textAlign: TextAlign.left,
+                )),
+            SizedBox(height: 20 / 4),
+            _buildSearch(),
+            _buildContactList(itemCount)
+          ],
         ));
   }
 
+  void sortContact(BuildContext context) {
+    return setState(() {
+      allContacts
+          .map((Contact contacts) => Card(
+                child: ListTile(
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(contacts.user,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16)),
+                      Text(toggleSwitch(isSwitched, contacts)),
+                    ],
+                  ),
+                  subtitle: Text("0" + contacts.phone.toString()),
+                  onTap: () => share(context, contacts),
+                ),
+              ))
+          .toList();
+      allContacts.sort((a, b) => b.checkIn.compareTo(a.checkIn));
+    });
+  }
+
+  Widget _buildSearch() => SearchWidget(
+        text: query,
+        hintText: 'Search',
+        onChanged: searchContact,
+      );
+
+  void searchContact(String query) {
+    final contacts = allContacts.where((contact) {
+      final userLower = contact.user.toLowerCase();
+      final phoneLower = contact.phone.toString().toLowerCase();
+      final searchLower = query.toLowerCase();
+
+      return userLower.startsWith(searchLower) ||
+          phoneLower.startsWith(searchLower);
+    }).toList();
+
+    setState(() {
+      this.query = query;
+      this.contacts = contacts;
+    });
+  }
+
+  Widget _buildContactList(int itemCount) => Expanded(
+          child: RefreshIndicator(
+        key: refreshKey,
+        onRefresh: () async {
+          await refreshList();
+        },
+        child: ReorderableListView.builder(
+            // scrollController: _scrollController,
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            itemCount: itemCount,
+            onReorder: (oldIndex, newIndex) => setState(() {
+                  final index = newIndex > oldIndex ? newIndex - 1 : newIndex;
+                  final contact = contacts.removeAt(oldIndex);
+                  contacts.insert(index, contact);
+                }
+                ),
+            itemBuilder: (context, index) {
+              final contact = contacts[index];
+              return buildContact(index, contact);
+            }
+            ),
+      ));
+
+  Widget buildContact(int index, Contact contact) => ListTile(
+        key: ValueKey(contact),
+        title: Padding(
+          padding: const EdgeInsets.only(bottom: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(contact.user,
+                  style: TextStyle(
+                      color: Colors.purple,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18)),
+              Text(toggleSwitch(isSwitched, contact),
+                  style: TextStyle(color: Colors.grey, fontSize: 14)),
+            ],
+          ),
+        ),
+        subtitle: Text("0" + contact.phone.toString(),
+            style: TextStyle(color: Colors.grey, fontSize: 16)),
+        onTap: () => share(context, contact),
+      );
 
   void share(BuildContext context, Contact contact) {
     final RenderBox box = context.findRenderObject();
@@ -143,5 +187,16 @@ class _ContactWithListState extends State<ContactWithList> {
     } else {
       return normal;
     }
+  }
+
+  Future<Null> refreshList() async {
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      itemCount = itemCount + 15;
+
+    });
+
+    return _buildContactList(itemCount);
+
   }
 }
